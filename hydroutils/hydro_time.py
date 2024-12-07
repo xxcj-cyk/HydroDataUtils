aa370578fe6e3a7d2784a3db75a744267edbd914
xxcj <cyk19951112@gmail.com>
2024-12-07 16:50:06 +0800

    lots of fundition

---
+++ hydroutils/hydro_time.py
@@ -0,0 +1,23 @@
+import datetime
+from typing import Union
+import numpy as np
+
+
+def t_range_days(t_range, *, step=np.timedelta64(1, "D")) -> np.array:
+    """
+    Transform the two-value t_range list to a uniformly-spaced list (default is a daily list).
+    For example, ["2000-01-01", "2000-01-05"] -> ["2000-01-01", "2000-01-02", "2000-01-03", "2000-01-04"]
+    Parameters
+    ----------
+    t_range
+        two-value t_range list
+    step
+        the time interval; its default value is 1 day
+    Returns
+    -------
+    np.array
+        a uniformly-spaced (daily) list
+    """
+    sd = datetime.datetime.strptime(t_range[0], "%Y-%m-%d")
+    ed = datetime.datetime.strptime(t_range[1], "%Y-%m-%d")
+    return np.arange(sd, ed, step)
