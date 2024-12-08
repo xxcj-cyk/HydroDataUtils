cd5bde7d18925e765e98f639345b21d6649db859
xxcj <cyk19951112@gmail.com>
2024-12-08 22:50:06 +0800

    version1.0

---
+++ hydroutils/hydro_file.py
@@ -0,0 +1,38 @@
+import os
+import platform
+
+
+def get_cache_dir(app_name="hydro", dataset_name=None):
+    home = os.path.expanduser("~")
+    system = platform.system()
+
+    if system == "Windows":
+        cache_dir = os.path.join(home, "AppData", app_name, "temp")
+    else:
+        cache_dir = os.path.join(home, ".cache", app_name, "temp")
+
+    if dataset_name:
+        cache_dir = os.path.join(cache_dir, dataset_name)
+
+    if not os.path.exists(cache_dir):
+        os.makedirs(cache_dir)
+
+    return cache_dir
+
+
+def get_dataset_dir(app_name="hydro", dataset_name=None):
+    home = os.path.expanduser("~")
+    system = platform.system()
+
+    if system == "Windows":
+        cache_dir = os.path.join(home, "AppData", app_name)
+    else:
+        cache_dir = os.path.join(home, ".cache", app_name)
+
+    if dataset_name:
+        cache_dir = os.path.join(cache_dir, dataset_name)
+
+    if not os.path.exists(cache_dir):
+        os.makedirs(cache_dir)
+
+    return cache_dir
--- hydroutils/hydro_time.py
+++ hydroutils/hydro_time.py
@@ -3,6 +3,15 @@ from typing import Union
 import numpy as np
 
 
+def t2str(t_: Union[str, datetime.datetime]):
+    if type(t_) is str:
+        return datetime.datetime.strptime(t_, "%Y-%m-%d")
+    elif type(t_) is datetime.datetime:
+        return t_.strftime("%Y-%m-%d")
+    else:
+        raise NotImplementedError("We don't support this data type yet")
+
+
 def t_range_days(t_range, *, step=np.timedelta64(1, "D")) -> np.array:
     """
     Transform the two-value t_range list to a uniformly-spaced list (default is a daily list).
@@ -18,6 +27,6 @@ def t_range_days(t_range, *, step=np.timedelta64(1, "D")) -> np.array:
     np.array
         a uniformly-spaced (daily) list
     """
-    sd = datetime.datetime.strptime(t_range[0], "%Y-%m-%d")
-    ed = datetime.datetime.strptime(t_range[1], "%Y-%m-%d")
-    return np.arange(sd, ed, step)
+    startdata = datetime.datetime.strptime(t_range[0], "%Y-%m-%d")
+    enddata = datetime.datetime.strptime(t_range[1], "%Y-%m-%d")
+    return np.arange(startdata, enddata, step)
