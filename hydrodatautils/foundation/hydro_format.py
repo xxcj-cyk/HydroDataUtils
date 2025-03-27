88db593568d99ac19ec2fd00e3a5d03be36c06ca
xxcj <cyk19951112@gmail.com>
2025-03-27 13:25:12 +0800

    json format

--- hydrodatautils/foundation/hydro_format.py
+++ hydrodatautils/foundation/hydro_format.py
@@ -1,8 +1,18 @@
 import json
+from pathlib import Path
 
 def serialize_json(my_dict, my_file):
+    def convert_paths(obj):
+        if isinstance(obj, Path):
+            return str(obj)
+        elif isinstance(obj, dict):
+            return {k: convert_paths(v) for k, v in obj.items()}
+        elif isinstance(obj, (list, tuple)):
+            return [convert_paths(item) for item in obj]
+        return obj
+    
     with open(my_file, "w") as FP:
-        json.dump(my_dict, FP, indent=4)
+        json.dump(convert_paths(my_dict), FP, indent=4)
 
 
 def unserialize_json(my_file):
